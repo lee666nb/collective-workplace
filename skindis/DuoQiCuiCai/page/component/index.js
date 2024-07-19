d5df958cc23f5c9e262c567ec8dcd1cb56ae5401
cty-11-y <2734649446@qq.com>
2024-07-19 11:59:57 +0800

    改了首页

--- skindis/DuoQiCuiCai/page/component/index.js
+++ skindis/DuoQiCuiCai/page/component/index.js
@@ -4,8 +4,22 @@ Page({
     autoplay: false,
     interval: 3000,
     duration: 800,
-  }
-  
+  },
+  navigateToDiagnose() {  
+    wx.reLaunch({  
+      url: '/page/component/xianwei/xianwei' // 假设你有一个diagnose页面  
+    });  
+  },  
+  navigateToKnowledge() {  
+    wx.reLaunch({  
+      url: '/page/component/run/run' // 假设你有一个knowledge页面  
+    });  
+  },  
+  navigateToProfile() {  
+    wx.reLaunch({  
+      url: '/page/component/user/user' // 假设你有一个profile页面  
+    });  
+  }  
   
 })
 
