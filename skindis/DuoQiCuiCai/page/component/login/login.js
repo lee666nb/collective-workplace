160f015a76ff7040656057f3a06b7ed6e1c6329d
cty-11-y <2734649446@qq.com>
2024-07-24 15:20:51 +0800

    首页改动

--- skindis/DuoQiCuiCai/page/component/login/login.js
+++ skindis/DuoQiCuiCai/page/component/login/login.js
@@ -1,8 +1,27 @@
+const app = getApp()
+ let username=''
+ let password=''
+ let qemail=''
 Page({
   data: {
-    tabIndex: 0 // tab下标
+    tabIndex: 0 ,// tab下标
+    username: '',
+    password: '',
+    qemail: '',
+    clientHeight:'',
+    pwdtype: true,
+    image: '/image/closesee.png'
   },
-  
+    //获取输入款内容
+    content(e){
+        username=e.detail.value
+      },
+      password(e){
+        password=e.detail.value
+      },
+      email(e){
+          qemail=e.detail.value
+      },
   // tab点击
   tabClick: function(e) {
     this.setData({
@@ -18,12 +37,7 @@ Page({
     });
   },
 
-  // 注册
-  register: function() {
-    wx.navigateTo({
-      url: '/page/register/register'
-    });
-  },
+ 
   
   // 登录
   login: function() {
@@ -31,5 +45,86 @@ Page({
       title: "登录",
       icon: "none"
     });
-  }
+  },
+    //登录事件
+    goadmin(){
+        if(username=='')
+        {
+          wx.showToast({
+            icon:'none',
+            title: '账号不能为空',
+          })
+        }else if(password==''){
+          wx.showToast({
+            icon:'none',
+            title: '密码不能为空',
+          })
+        }else if(username=='admin'&&password=='123456'){
+            username='';
+            password='';
+            wx.showToast({
+                title: "登录成功",   
+                icon: 'success',   
+                duration: 1000,        
+                })           
+                setTimeout(function () {         
+                wx.reLaunch({          
+                url: '/page/component/index',          
+                })       
+                }, 1000)
+        }else{
+            wx.showToast({
+                icon:'none',
+                title: '账号或者密码错误',
+              })
+        }
+    
+      },
+      goadminx(){
+        if(qemail=='')
+        {
+          wx.showToast({
+            icon:'none',
+            title: '邮箱不能为空',
+          })
+        }else if(password==''){
+          wx.showToast({
+            icon:'none',
+            title: '密码不能为空',
+          })
+        }else if(qemail=='2734649446@qq.com'&&password=='123456'){
+            qemail='';
+            password='';
+            wx.showToast({
+                title: "登录成功",   
+                icon: 'success',   
+                duration: 1000,        
+                })           
+                setTimeout(function () {         
+                wx.reLaunch({          
+                url: '/page/component/index',          
+                })       
+                }, 1000)
+        }else{
+            wx.showToast({
+                icon:'none',
+                title: '账号或者密码错误',
+              })
+        }
+    
+      },
+      goto() {
+        if (this.data.pwdtype == true) {
+          this.setData({
+            pwdtype: false,
+            image: '/image/opensee.png'
+          })
+        } else {
+          this.setData({
+            pwdtype: true,
+            image: '/image/closesee.png'
+          })
+        }
+     
+      }
 })
