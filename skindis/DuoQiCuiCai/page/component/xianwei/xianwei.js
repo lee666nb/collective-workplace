10036fe8a20d19d572f601cfa5b985b22fa7ddc5
liyi <139129954+lee666nb@users.noreply.github.com>
2024-07-26 11:51:59 +0800

    预测界面改了

--- skindis/DuoQiCuiCai/page/component/xianwei/xianwei.js
+++ skindis/DuoQiCuiCai/page/component/xianwei/xianwei.js
@@ -1,66 +1,29 @@
-// page/component/category/category.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    imagePath: ''
+  },
+  
+  chooseImage: function() {
+    const that = this;
+    wx.chooseImage({
+      count: 1, // 可以选择的图片数量
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机
+      success: function(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths;
+        that.setData({
+          imagePath: tempFilePaths[0]
+        });
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  startPrediction: function() {
+    wx.showModal({
+      title: '预测结果',
+      content: '这是皮肤病。',
+      showCancel: false
+    });
   }
-})
+});
