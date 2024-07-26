343590e2d8de4b67c728aae4968794350004e89c
liyi <139129954+lee666nb@users.noreply.github.com>
2024-07-26 12:02:58 +0800

    看description，里面写了这次更改的变化
    点击按钮或者按钮下面的一块区域能弹出对话框让用户选择图片，用户选择完图片后，在按钮下面的那块区域可以看见上传的图片，再点击这块区域的下方的开始预测按钮，出现一个对话框，内容写着这是皮肤病。还有两个暂时没有用open cv，用的js实现的亮度，对比度调节

--- skindis/DuoQiCuiCai/page/component/xianwei/xianwei.js
+++ skindis/DuoQiCuiCai/page/component/xianwei/xianwei.js
@@ -1,22 +1,97 @@
 Page({
   data: {
-    imagePath: ''
+    imagePath: '',
+    brightness: 0,
+    contrast: 1
   },
-  
+
   chooseImage: function() {
     const that = this;
     wx.chooseImage({
-      count: 1, // 可以选择的图片数量
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
       success: function(res) {
-        // tempFilePath可以作为img标签的src属性显示图片
         const tempFilePaths = res.tempFilePaths;
         that.setData({
           imagePath: tempFilePaths[0]
+        }, () => {
+          that.updateImage();
         });
       }
     });
+  },
+
+  onBrightnessChange: function(e) {
+    this.setData({
+      brightness: e.detail.value
+    });
+    this.updateImage();
+  },
+
+  onContrastChange: function(e) {
+    this.setData({
+      contrast: e.detail.value
+    });
+    this.updateImage();
+  },
+
+  updateImage: function() {
+    const that = this;
+    const ctx = wx.createCanvasContext('imageCanvas');
+    wx.getImageInfo({
+      src: this.data.imagePath,
+      success: function(res) {
+        const width = res.width;
+        const height = res.height;
+        ctx.drawImage(that.data.imagePath, 0, 0, width, height);
+        ctx.draw(false, () => {
+          wx.canvasGetImageData({
+            canvasId: 'imageCanvas',
+            x: 0,
+            y: 0,
+            width: width,
+            height: height,
+            success: function(res) {
+              const imageData = res.data;
+              const adjustedData = that.adjustImageData(imageData, width, height, that.data.brightness, that.data.contrast);
+              wx.canvasPutImageData({
+                canvasId: 'imageCanvas',
+                data: adjustedData,
+                x: 0,
+                y: 0,
+                width: width,
+                height: height,
+                success: function() {
+                  wx.canvasToTempFilePath({
+                    canvasId: 'imageCanvas',
+                    success: function(result) {
+                      that.setData({
+                        imagePath: result.tempFilePath
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
+        });
+      }
+    });
+  },
+
+  adjustImageData: function(imageData, width, height, brightness, contrast) {
+    const data = new Uint8ClampedArray(imageData);
+    for (let i = 0; i < data.length; i += 4) {
+      for (let j = 0; j < 3; j++) { // 调整R、G、B通道
+        data[i + j] = this.clamp(data[i + j] * contrast + brightness);
+      }
+    }
+    return data;
+  },
+
+  clamp: function(value) {
+    return Math.max(0, Math.min(255, value));
   },
 
   startPrediction: function() {
